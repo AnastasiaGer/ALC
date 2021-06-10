4dc16d954228527acbd00e7c425c4c75cae9c5bc
AnastasiaGer <aagermanova@gmail.com>
2021-06-09 18:40:23 -0700

    done

---
+++ public/js/script.js
@@ -0,0 +1,38 @@
+$(function(){
+
+  $('.tab').on('click', function(e){
+    e.preventDefault()
+
+    $($(this).siblings()).removeClass('tab--active');
+    $($(this).closest('.tabs-wrapper').siblings().find('div')).removeClass('tabs-content--active');
+
+    $(this).addClass('tab--active');
+    $($(this).attr('href')).addClass('tabs-content--active');
+
+    $('.product-slider').slick('setPosition');
+  });
+
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+
+  
+});
